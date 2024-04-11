2884e0f99d5ec81164fd028d82b8bb65b5f82a8e
Everton Dourado <116851307+everdourado@users.noreply.github.com>
2024-04-11 15:31:26 -0300

    Sugestão de código comentado

--- item.js
+++ item.js
@@ -11,4 +11,33 @@ class Item {
   }
   
   module.exports = Item;
+
+/*
+class atualizarAgenda {
+  constructor(tarefa, estado) {
+    this._tarefa = tarefa;
+    this._estado = estado;
+  }
+
+  get tarefa() {
+    return this._tarefa;
+  }
+
+  get estado() {
+    return this._estado;
+  }
+  
+  concluirAtualizacao() {
+  if (!this._tarefa || !this._estado) {
+    throw new Error('Tarefa e estado são obrigatórios');
+  } else {
+    return { tarefa: this._tarefa, estado: this._estado };
+  }
+}
+
+}
+
+module.exports = atualizarAgenda;
+
+*/
   
