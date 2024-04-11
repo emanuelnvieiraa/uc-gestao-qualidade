664f673bca57f74eb39875629881c9be78996ce7
Everton Dourado <116851307+everdourado@users.noreply.github.com>
2024-04-11 19:12:32 -0300

    Update item.test.js

--- item.test.js
+++ item.test.js
@@ -1,22 +1,3 @@
-const Item = require('./item.js');
-
-describe('Testes dos itens', () => {
-  it('Deve ter 2 campos: nome e estado', () => {
-    const item = new Item('Ir para a academia','Concluída');
-
-    expect(item.nome).toBe('Ir para a academia');
-    expect(item.valor).toBe('Concluída');
-   
-  });
-
-  it('Deve ter os dois valores declarados caso não tiver apresentar um erro', () => {
-    const item = new Item('Ir para a academia', '');
-
-    expect(new Set(Item)).toContain('Concluída');
-  });
-});
-
-/*
 const AtualizarAgenda = require('./item.js');
 
 describe('Testes dos itens na atualização da agenda', () => {
@@ -35,5 +16,3 @@ describe('Testes dos itens na atualização da agenda', () => {
     });
   });
 });
-
-*/
