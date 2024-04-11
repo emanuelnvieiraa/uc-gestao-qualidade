12fbaa7312b6010a15fe36f7833324baf9f7da54
Everton Dourado <116851307+everdourado@users.noreply.github.com>
2024-04-11 15:42:57 -0300

    Sugestão de código comentado item.test.js

--- item.test.js
+++ item.test.js
@@ -15,3 +15,24 @@ describe('Testes dos itens', () => {
     expect(new Set(Item)).toContain('Concluída');
   });
 });
+
+/*
+const atualizarAgenda = require('./item.js');
+
+describe('Testes dos itens na atualização da agenda', () => {
+  it('Deve ter 2 campos: nome e estado', () => {
+    const item = new Item('Ir para a academia','Concluída');
+
+    expect(item.nome).toBe('Ir para a academia');
+    expect(item.valor).toBe('Concluída');
+   
+  });
+
+describe('Testes da atualização com falha', () => {
+  it('Deve retornar erro quando um ou amos itens não forem fornecidos', () => {
+    const primeiroItem = new atualizarAgenda('Alimentar gatos às 10');
+
+    expect(() => carrinho.finalizaCompra()).toThrowError('Tarefa e estado são de preenchimento obrigatório');
+  });
+});
+*/
