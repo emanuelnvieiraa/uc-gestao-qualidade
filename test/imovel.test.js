690ef497497f95be7bc4d5025f0799240bf7d5b9
EverDourado <evertondmcf@hotmail.com>
2024-06-06 22:49:52 -0300

    Testes com Jest: Listar user e imovel; Criar Imóvel

--- test/imovel.test.js
+++
@@ -1,5 +0,0 @@
-test("Listar todos os imóveis", async() => {
-    const response = await request(app).get('/imovel');
-    expect(response.status).toBe(200);
-    expect(response.body.lenght).toBeGreaterThan(0);
-});
--- test/user.test.js
+++
@@ -1,5 +0,0 @@
-test("Listar todos os usuários", async() => {
-    const response = await request(app).get('/');
-    expect(response.status).toBe(200);
-    expect(response.body.lenght).toBeGreaterThan(0);
-});
